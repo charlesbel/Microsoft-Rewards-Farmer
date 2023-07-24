c943afd74e7bb7698c6b513f451d01a6e625def1
Charles Bel <admin@gooddev.fr>
2023-07-24 14:16:21 +0200

    Simplifications

--- lib/punchCards.py
+++ lib/punchCards.py
@@ -7,38 +7,26 @@ from selenium.webdriver.common.by import By
 
 from .constants import BASE_URL
 from .utils import Utils
-
+from .activities import Activities
 
 class PunchCards:
     def __init__(self, browser: WebDriver):
         self.browser = browser
         self.utils = Utils(browser)
+        self.activities = Activities(browser)
 
     def completePunchCard(self, url: str, childPromotions: dict):
         self.browser.get(url)
         for child in childPromotions:
             if child['complete'] == False:
+                self.browser.find_element(By.CLASS_NAME, 'offer-cta').click()
                 if child['promotionType'] == "urlreward":
-                    self.browser.execute_script(
-                        "document.getElementsByClassName('offer-cta')[0].click()")
                     self.utils.visitNewTab(random.randint(13, 17))
-                if child['promotionType'] == "quiz":
-                    self.browser.execute_script(
-                        "document.getElementsByClassName('offer-cta')[0].click()")
+                elif child['promotionType'] == "quiz":
                     self.utils.switchToNewTab(8)
-                    counter = str(self.browser.find_element(
-                        By.XPATH, '//*[@id="QuestionPane0"]/div[2]').get_attribute('innerHTML'))[:-1][1:]
-                    numberOfQuestions = max(
-                        [int(s) for s in counter.split() if s.isdigit()])
-                    for question in range(numberOfQuestions):
-                        self.browser.execute_script(
-                            f'document.evaluate("//*[@id=\'QuestionPane{question}]/div[1]/div[2]/a[{random.randint(1, 3)}]/div", document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.click()')
-                        time.sleep(5)
-                        self.browser.find_element(
-                            By.XPATH, f'//*[@id="AnswerPane{question}"]/div[1]/div[2]/div[4]/a/div/span/input').click()
-                        time.sleep(3)
-                    time.sleep(5)
-                    self.utils.closeCurrentTab()
+                    self.activities.completeABC()
+                else:
+                    self.utils.visitNewTab(random.randint(13, 17))
 
     def completePunchCards(self):
         self.completePromotionalItems()
